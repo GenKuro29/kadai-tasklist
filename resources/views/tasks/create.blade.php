545d7b160e76aa7c362faf95005d9ba567dc72c7
Gen.K <kamakuraha1192@hotmail.com>
2018-07-13 13:56:30 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,29 +2,30 @@
 
 @section('content')
 
+    
     <h1>タスクの新規登録ページ</h1>
-    
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
-    
-        {!! Form::Label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}<br>
-        
-        {!! Form::Label('status', 'ステータス:') !!}
-
-        
-        {!! Form::select('status',[
-            'open' => 'open',
-            'ongoing' => 'ongoing',
-            'completed' => 'completed',
-            'invalid' => 'invalid']
-        ) !!}<br>
-        
-        
-        
-        {!! Form::submit('作成') !!}
-        
-        
-    
-    {!! Form::close() !!}
-
+     <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+                
+                <div class="form-group">
+                    {!! Form::Label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}<br>
+                </div>
+                    
+                <div class="form-group">
+                    {!! Form::Label('status', 'ステータス:') !!}
+                    {!! Form::select('status', [
+                        'open' => 'open',
+                        'ongoing' => 'ongoing',
+                        'completed' => 'completed',
+                        'invalid' => 'invalid'],
+                        'open',
+                        ['class' => 'form-control']
+                        ) !!}<br>
+                </div>
+                {!! Form::submit('作成', ['class' => 'btn btn-primary'])!!}
+            {!! Form::close() !!}
+        </div>
+    </div>
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,21 +4,29 @@
 
     <h1>id: {{ $task->id }}のメッセージ編集ページ</h1>
     
-    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method'=> 'put']) !!}
-    
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}<br>
-        
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::select('status',[
-            'open' => 'open',
-            'ongoing' => 'ongoing',
-            'completed' => 'completed',
-            'invalid' => 'invalid']
-        ) !!}<br>
-        
-        {!! Form::submit('更新') !!}
-        
-    {!! Form::close() !!}
 
+    <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+            {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method'=> 'put']) !!}
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}<br>
+                </div>
+                
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::select('status',[
+                        'open' => 'open',
+                        'ongoing' => 'ongoing',
+                        'completed' => 'completed',
+                        'invalid' => 'invalid'],
+                        null,
+                        ['class' => 'form-control']
+                    ) !!}<br>
+                </div>
+                
+                {!! Form::submit('更新',['class' => 'btn btn-default']) !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
 @endsection
